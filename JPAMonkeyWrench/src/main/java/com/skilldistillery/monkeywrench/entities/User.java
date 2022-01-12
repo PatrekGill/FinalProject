62550e3e3b704577247e86673771d38d48130f61
Patrek Gill <patrekgill@yahoo.com>
2022-01-12 01:31:26 -0700

    added user entity with tests

--- JPAMonkeyWrench/src/main/java/com/skilldistillery/monkeywrench/entities/User.java
+++ JPAMonkeyWrench/src/main/java/com/skilldistillery/monkeywrench/entities/User.java
@@ -1,5 +1,109 @@
 package com.skilldistillery.monkeywrench.entities;
 
-public class User {
+import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+		
+	private String username;
+	private String password;
+	private boolean enabled;
+	private String role;
+	
+
+	/* ----------------------------------------------------------------------------
+		Constructors
+	---------------------------------------------------------------------------- */
+	public User() {}
+
+
+	/* ----------------------------------------------------------------------------
+		Get/Set ID
+	---------------------------------------------------------------------------- */
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	/* ----------------------------------------------------------------------------
+		Get/Set username
+	---------------------------------------------------------------------------- */
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+
+	/* ----------------------------------------------------------------------------
+		Get/Set password
+	---------------------------------------------------------------------------- */
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+	/* ----------------------------------------------------------------------------
+		Get/Set enabled
+	---------------------------------------------------------------------------- */
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+
+	/* ----------------------------------------------------------------------------
+		Get/Set role
+	---------------------------------------------------------------------------- */
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	/* ----------------------------------------------------------------------------
+		Misc
+	---------------------------------------------------------------------------- */
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + "]";
+	}
+	
+	
 }
