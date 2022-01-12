d0d625392dd83f59c1d94f2b47e228fa720300ff
Patrek Gill <patrekgill@yahoo.com>
2022-01-12 02:04:24 -0700

    added user registration, updated defaults in User table

--- MonkeyWrench/src/main/java/com/skilldistillery/monkeywrench/controllers/AuthController.java
+++ MonkeyWrench/src/main/java/com/skilldistillery/monkeywrench/controllers/AuthController.java
@@ -1,7 +1,14 @@
 package com.skilldistillery.monkeywrench.controllers;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.monkeywrench.entities.User;
@@ -16,5 +23,22 @@ public class AuthController {
 	@GetMapping("usertest")
 	public User userTest() {
 		return authSvc.findUserByName("johndoe");
+	}
+	
+	@RequestMapping(path = "/register", method = RequestMethod.POST)
+	public User register(@RequestBody User user, HttpServletResponse res) {
+
+	    if (user == null) {
+	        res.setStatus(400);
+	    }
+
+	    user = authSvc.register(user);
+
+	    return user;
+	}
+
+	@RequestMapping(path = "/authenticate", method = RequestMethod.GET)
+	public User authenticate(Principal principal) {
+	    return authSvc.findUserByName(principal.getName());
 	}
 }
