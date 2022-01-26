ac394fe634c50912f041493d0e5c4c1baa0e61af
PatrekGill <patrekGill@yahoo.com>
2022-01-25 18:14:58 -0700

    added address tests

---
+++ JPAMonkeyWrench/src/test/java/com/skilldistillery/monkeywrench/entities/AddressTest.java
@@ -0,0 +1,86 @@
+package com.skilldistillery.monkeywrench.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAMonkeyWrench");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		address = null;
+	}
+
+	@Test
+	void test_address_city_mapping() {
+		assertNotNull(address);
+		assertEquals("Hanover",address.getCity());
+	}
+	
+	@Test
+	void test_address_street_mapping() {
+		assertNotNull(address);
+		assertEquals("62 Fake St.",address.getStreet());
+	}
+	
+	@Test
+	void test_address_street2_mapping() {
+		assertNotNull(address);
+		assertEquals("unit 1",address.getStreet2());
+	}
+	
+	@Test
+	void test_address_stateAbbv_mapping() {
+		assertNotNull(address);
+		assertEquals("PA",address.getStreet2());
+	}
+	
+	@Test
+	void test_address_zipcode_mapping() {
+		assertNotNull(address);
+		assertEquals(99022,address.getZipCode());
+	}
+	
+	@Test
+	void test_address_user_mapping() {
+		assertNotNull(address);
+		assertNotNull(address.getUser());
+		assertEquals(1,address.getUser().getId());
+	}
+	
+	@Test
+	void test_address_notes_mapping() {
+		assertNotNull(address);
+		assertNotNull(address.getNotes());
+		assertEquals("notes",address.getNotes());
+	}
+
+}
