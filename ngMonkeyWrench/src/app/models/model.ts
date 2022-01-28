06fc1d951f07c5af68a8536d7a83aa288710d9cb
CJ Harris <Carroll.Harris2@gmail.com>
2022-01-27 19:23:24 -0500

    model service and model build in ng, finished servicetypetest junit test

--- ngMonkeyWrench/src/app/models/model.ts
+++ ngMonkeyWrench/src/app/models/model.ts
@@ -1,2 +1,15 @@
 export class Model {
+  id: number;
+  name: string | undefined;
+  modelNumber: string | undefined;
+  description: string | undefined;
+  fuelType: string | undefined;
+
+  constructor(id: number = 0, name?: string, modelNumber?: string, description?: string, fuelType?: string){
+    this.id = id;
+    this.name = name;
+    this.modelNumber = modelNumber;
+    this.description = description;
+    this.fuelType = fuelType;
+  }
 }
