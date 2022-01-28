809fea54ded863cd6384ea07721c801f6d19cf9b
CJ Harris <Carroll.Harris2@gmail.com>
2022-01-28 17:15:47 -0500

    nav bar register and log out added

--- ngMonkeyWrench/src/app/component/contractor-login/contractor-login.component.ts
+++ ngMonkeyWrench/src/app/component/contractor-login/contractor-login.component.ts
@@ -9,30 +9,31 @@ import { ServiceCall } from 'src/app/models/service-call';
 export class ContractorLoginComponent implements OnInit {
 
   expandedBox: boolean = false;
-  testServiceCall: ServiceCall[] = [{
-    problemDescription: "No fridge or furnace",
-    dateCreated: "2022-01-15T10:00:00",
-    dateScheduled: "2022-01-16T11:00:00",
-    hoursLabor: 0,
-    totalCost: 1.99,
-    contractorNotes: null,
-    address: {
-        id: 2
-    },
-    problem: {
-        id: 2
-    },
-    busines: {
-        id: 1
-    },
-    user: {
-        id: 1
-    },
-    completed: false,
-    estimate: true,
-    customerRating: null,
-    customerRatingComment: null
-  }]
+
+  // testServiceCall: ServiceCall[] = [{
+  //   problemDescription: "No fridge or furnace",
+  //   dateCreated: "2022-01-15T10:00:00",
+  //   dateScheduled: "2022-01-16T11:00:00",
+  //   hoursLabor: 0,
+  //   totalCost: 1.99,
+  //   contractorNotes: null,
+  //   address: {
+  //       id: 2
+  //   },
+  //   problem: {
+  //       id: 2
+  //   },
+  //   busines: {
+  //       id: 1
+  //   },
+  //   user: {
+  //       id: 1
+  //   },
+  //   completed: false,
+  //   estimate: true,
+  //   customerRating: null,
+  //   customerRatingComment: null
+  // }]
   // contractorServiceCallsInProgress: ServiceCall[];
 
   constructor() { }
