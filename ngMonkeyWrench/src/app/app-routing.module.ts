f92e3ef8b5b095878133f4e832c11e05c98d020c
PatrekGill <patrekGill@yahoo.com>
2022-01-29 02:10:48 -0700

    seperated sidebar monkey image styling into relevant css file

--- ngMonkeyWrench/src/app/app-routing.module.ts
+++ ngMonkeyWrench/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BusinessComponent } from './component/business/business.component';
+import { ContractorLoginComponent } from './component/contractor-login/contractor-login.component';
 import { HomeComponent } from './component/home/home.component';
 import { UserComponent } from './component/user/user.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'user', component: UserComponent },
+  { path: 'contractor', component: ContractorLoginComponent },
   { path: 'business', component: BusinessComponent },
   { path: '**', component: HomeComponent }
 ];
