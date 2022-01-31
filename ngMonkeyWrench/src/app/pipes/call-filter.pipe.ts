ba5d1c24ae41f792432da600469b8e0ef68330e5
Kyle Palasti <Kyle.Palasti@gmail.com>
2022-01-31 15:50:18 -0700

    filterByPipe complete

--- ngMonkeyWrench/src/app/pipes/call-filter.pipe.ts
+++ ngMonkeyWrench/src/app/pipes/call-filter.pipe.ts
@@ -1,3 +1,4 @@
+import { Call } from '@angular/compiler';
 import { Pipe, PipeTransform } from '@angular/core';
 import { ServiceCall } from '../models/service-call';
 
@@ -16,25 +17,36 @@ export class CallFilterPipe implements PipeTransform {
     }
     else if(filterBy == "month"){
       let filteredCalls: ServiceCall[] = [];
-      calls.forEach(ServiceCall => {
-        if(ServiceCall.dateScheduled){
-          if(new Date(ServiceCall.dateScheduled).getMonth() === month){
-            filteredCalls.push(ServiceCall);
+      calls.forEach(call => {
+        if(call.dateScheduled){
+          if(new Date(call.dateScheduled).getMonth() === month){
+            filteredCalls.push(call);
           }
         }
         });
       return filteredCalls;
     }
     else if(filterBy == "week"){
+      //Calculate low and high range as iterating through service calls
+      let delta = 1000 * 60 * 60 * 84;
+      let threeDaysPrior = date.getTime() - delta;
+      let threeDaysFuture = date.getTime() + delta;
       let filteredCalls: ServiceCall[] = [];
+      calls.forEach(call => {
+        if(call.dateScheduled){
+          if(new Date(call.dateScheduled).getTime() > threeDaysPrior && new Date(call.dateScheduled).getTime() < threeDaysFuture){
+            filteredCalls.push(call);
+        }
+      }
+    });
       return filteredCalls;
-    }
+  }
     else if(filterBy == "day"){
       let filteredCalls: ServiceCall[] = [];
-      calls.forEach(ServiceCall =>{
-        if(ServiceCall.dateScheduled){
-          if(new Date(ServiceCall.dateScheduled).getDay() === day){
-            filteredCalls.push(ServiceCall);
+      calls.forEach(call =>{
+        if(call.dateScheduled){
+          if(new Date(call.dateScheduled).getDay() === day){
+            filteredCalls.push(call);
           }
         }
       })
